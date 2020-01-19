ed6b68ff66b8a20d4083f370e82b036c05036f0d
Kina Rain <tina@lawtk.net>
2020-01-18 22:08:08 -0500

    finished js files and functions

--- db_functions.js
+++ db_functions.js
@@ -8,6 +8,170 @@ module.exports.add_department = function (departmentName, connection) {
     })
 }
 
-module.exports.add_role = function(userResponse) {
-    console.log(userResponse);
+module.exports.get_managers = function(connection, callback) {
+    let managerSQL = `
+    SELECT DISTINCT CONCAT(mgr.first_name, " ", mgr.last_name) as manager
+    FROM employee e
+    JOIN employee mgr ON mgr.id = e.manager_id
+    `
+    connection.query(managerSQL, function(err, res) {
+        
+        if (err) callback(err, null);
+
+        let managers = [];
+
+        for (i in res) {
+            managers.push(res[i].manager);
+        }
+
+        //this will let the user add a new manager
+        managers.push("Add as manager");
+
+        callback(null, managers);
+    });
+}
+
+module.exports.add_employee = function(fName, lName, role, manager, connection) {
+
+    let roleReqSQL = `SELECT id FROM employee_role WHERE title = "${role}"`;
+
+    connection.query(roleReqSQL, function(err, roles) {
+        if (err) throw err;
+
+        let roleID = roles[0].id;
+
+        if (manager === "Add as manager") {
+            let employeeAddSQL = `
+            INSERT INTO employee (first_name, last_name, role_id)
+            VALUES ("${fName}", "${lName}", ${roleID})
+            `
+            connection.query(employeeAddSQL, function(err, res) {
+                if (err) throw err;
+
+                return res;
+            });
+        } else {
+            let managerGetSQL = `Select id from employee where CONCAT(first_name, " ", last_name) = "${manager}"`;
+
+            connection.query(managerGetSQL, function(err, manager) {
+                if (err) throw err;
+
+                let managerID = manager[0].id;
+
+                let employeeAddSQL = `
+                INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                VALUES ("${fName}", "${lName}", ${roleID}, ${managerID})
+                `
+                connection.query(employeeAddSQL, function(err, res) {
+                    if (err) throw err;
+
+                    return res;
+                });
+            });
+        }
+    });
+}
+
+module.exports.add_role = function(title, salary, department, connection) {
+   
+    connection.query("SELECT id, department_name FROM department", function(err, departments) {
+
+        if (err) throw err;
+
+        //go through the departments to find the right ID
+        for (let i in departments) {
+            if (departments[i].department_name === department) {
+                let roleInsert = `
+                INSERT INTO employee_role (title, salary, department_id)
+                VALUES ("${title}", ${salary}, ${departments[i].id})
+                `
+
+                connection.query(roleInsert, function(err, res) {
+                    if (err) throw err;
+
+                    //return the response so the function will end
+                    return res;
+                });
+            }
+        }
+    });
+}
+
+module.exports.view_departments = function(connection, display, callback) {
+
+    //set our SQL to get the departments data
+    let deptSQL = `SELECT id, department_name FROM department;`;
+
+    //create our array for the results
+    let dataResults = [];
+
+    //execute the sql to return the departments
+    connection.query(deptSQL, function(err, res) {
+        if (err) callback(err, null);
+
+        if(display) {
+            //go through the rows and push them into the array for console.table to render
+            for (let i in res) {
+                dataResults.push({departmentID: res[i].id.toString(), department_name: res[i].department_name});
+            }
+        } else {
+            for (let i in res) {
+                dataResults.push(res[i].department_name);
+            }
+        }
+
+            //send the data back
+            callback(null, dataResults);
+    });
+}
+
+module.exports.view_employees = function(connection, callback) {
+
+    let employeeSQL = `
+    SELECT e.first_name, e.last_name, er.title, er.salary, ifnull(concat(mgr.first_name,"", mgr.last_name), "") as manager, department_name
+    FROM employee e
+    JOIN employee_role er on e.role_id = er.id
+    JOIN department d on er.department_id = d.id
+    LEFT JOIN employee mgr on mgr.id = e.manager_id
+    `
+
+    let employees = [];
+
+    connection.query(employeeSQL, function(err, res) {
+        if (err) callback(err, null);
+
+        for (let i in res) {
+          employees.push({Name: res[i].first_name + " " + res[i].last_name, Title: res[i].title, Salary: res[i].salary, Manager: res[i].manager, Department: res[i].department_name});
+        }
+
+            callback(null, employees);
+        });
+        }
+    
+module.exports.view_employeeRoles = function(connection, display, callback) {
+
+    let roleSQL = `
+        SELECT title, salary, department_name
+        FROM employee_role er
+        JOIN department d on er.department_id = d.id
+    `
+
+    let employeeRoles = [];
+
+    connection.query(roleSQL, function(err, res) {
+        if (err) callback(err, null);
+
+        if (display) {
+            for (let i in res) {
+                employeeRoles.push({Role_Title: res[i].title, Salary: res[i].salary, Department: res[i].department_name});
+            }
+        } else {
+            for (let i in res) {
+                employeeRoles.push(res[i].title);
+            }
+        }
+
+        callback(null, employeeRoles);
+    });
+
 }
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const mysql = require("mysql");
 const dbFunc = require("./db_functions");
 const inquirer = require("inquirer");
-const questions = require("./questions")
+const questions = require("./questions");
+const table = require("console.table");
 
 const db_connection = mysql.createConnection({ 
     host: "localhost",
@@ -30,16 +31,89 @@ function start_program() {
         .then((response) => {
             //go through the response and call the various functions to view or update data
             if (response.function_list == "Add Department") {
-                add_department()
+                add_department();
+            }
+            if (response.function_list == "View Department") {
+                view_departments();
+            }
+            if (response.function_list == "View Employees") {
+                view_employees();
+            }
+            if (response.function_list == "View Employee Roles") {
+                view_employeeRoles();
+            }
+            if (response.function_list == "Add Employee Role") {
+                add_role();
+            }
+            if (response.function_list == "Add Employee") {
+                add_employee();
             }
             if (response.function_list == "Quit Program") {
+                console.log("");
+                console.log("******************************************************");
                 console.log("Thank you for using Employee Tracker, have a nice day!");
+                console.log("******************************************************");
+                console.log("");
                 return db_connection.end();
             }
         });
 }
 
+function add_employee() {
+    //prompt with employee questions
+    inquirer.prompt(questions.addEmployeeQuestion)
+        .then((response) => {
+            
+            let fName = response.first_name;
+            let lName = response.last_name;
+
+            //get the employee roles
+            dbFunc.view_employeeRoles(db_connection, false, function(err, roles) {
+                if (err) throw err;
+
+                let roleIDQuestion = {
+                    type: "list",
+                    name: "roleList",
+                    message: "Choose the role you want to assign the new employee to:",
+                    choices: roles
+                }
+                inquirer.prompt(roleIDQuestion)
+                    .then((response) => {
+
+                        let roleName = response.roleList;
+
+                        dbFunc.get_managers(db_connection, function(err, managers) {
+                            if (err) throw err;
+
+                            let managerIDQuestion = {
+                                type: "list",
+                                name: "managerList",
+                                message: "Choose the role you want to assign the new employee to:",
+                                choices: managers
+                            }
+                            inquirer.prompt(managerIDQuestion)
+                                .then((response) => {
+
+                                dbFunc.add_employee(fName, lName, roleName, response.managerList, db_connection);
+
+                                //tell the user that the roll was added.
+                                console.log("");
+                                console.log("");
+                                console.log("******************************************************");
+                                console.log("Employe " + fName + " " + lName + " was added");
+                                console.log("******************************************************");
+
+                                //go back to the beginning
+                                start_program();
+                                });
+                        });
+                    });
+            });
+        });
+}
+
 function add_department() {
+    //prompt with the department questions
     inquirer.prompt(questions.addDepartQuestion)
         .then((response) => {
             dbFunc.add_department(response.department_name, db_connection);
@@ -53,6 +127,89 @@ function add_department() {
             start_program();
         })
     
+}
+
+function add_role() {
+    //prompt with basic role questions
+    inquirer.prompt(questions.addRoleQuestion)
+    .then((response) => {
+
+        let role_title = response.role_title;
+        let role_salary = response.role_salary;
+
+        //get departments
+        dbFunc.view_departments(db_connection, false, function (err, departments) {
+            if (err) throw err
+
+            let deptQuestions = {
+                type: "list",
+                name: "departmentList",
+                message: "Choose the department you want to add the role to:",
+                choices: departments
+            }
+
+            inquirer.prompt(deptQuestions)
+                .then((response) => {
+                    dbFunc.add_role(role_title, role_salary, response.departmentList, db_connection);
+
+                    //tell the user that the role was added
+                    console.log("");
+                    console.log("");
+                    console.log("********************************************");
+                    console.log("Employee Role was added");
+                    console.log("********************************************");
+
+                    //go back to the beginning
+                    start_program();
+                });
+        });
+    });
+}
+
+function view_employeeRoles() {
+    dbFunc.view_employeeRoles(db_connection, true, function(err, roles) {
+        if (err) throw err;
+
+        console.log("");
+        console.log("");
+        console.log("********************************************");
+        console.table(roles);
+        console.log("********************************************");
+
+         //go back to the beginning
+         start_program();
+    });
+}
+
+function view_employees() {
+    dbFunc.view_employees(db_connection, function(err, employees) {
+        if(err) throw err;
+
+        console.log("");
+        console.log("");
+        console.log("********************************************");
+        console.table(employees);
+        console.log("********************************************");
+
+         //go back to the beginning
+         start_program();
+    });
+}
+
+function view_departments() {
+
+    dbFunc.view_departments(db_connection, true, function(err, departments) {
+        if (err) throw err;
+
+        console.log("");
+        console.log("");
+        console.log("********************************************");
+        console.table(departments);
+        console.log("********************************************");
+
+        //go back to the beginning
+        start_program();
+    });
 }
 
 function print_welcome() {
